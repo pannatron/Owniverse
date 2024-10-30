6aa7bdb5896a4b0f28bd79fd5e84692489d62378
pannatorn <bong.pannatorn@gmail.com>
2024-10-30 15:19:03 +0700

    updated solodity version 0.8.20

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,8 @@
 require("@nomiclabs/hardhat-waffle");
+require("dotenv").config(); // 
 
 module.exports = {
-  solidity: "0.8.0",
+  solidity: "0.8.20",
   networks: {
     localhost: {
       url: "http://127.0.0.1:8545"
